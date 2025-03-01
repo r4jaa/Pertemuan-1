563f2de2dcbd3f3ae3d3d2284375f71d1d2ecbde
r4jaa <164181764+r4jaa@users.noreply.github.com>
2025-03-01 23:43:06 +0700

    Add files via upload

---
+++ mahasiswa.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class mahasiswa {
+
+    private string nim;
+    private string nama;
+    private string prodi;
+    private ArrayList<matakuliah> listmatkul;
+    private dosen dosenwali;
+    private kendaraan kendaraan; 
+}
